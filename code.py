7f171b9df4e05ae6d6332979714956f59692b131
pgdbda66-debug <pgdbda66@gmail.com>
2025-09-14 16:39:16 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,3 +2,38 @@ print("Hello from Code file")
 
 
 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
+
+
+dbda@dbda-virtualbox:~$ cat case.sh
+
+#!/bin/bash
+
+while (True); do
+
+echo "1. date"
+
+echo "2. calendar"
+
+echo "3. list files"
+
+echo "4. Exit"
+
+read -p choose an option:" num
+
+case Snum in
+
+1)
+
+echo date is $(date)"
+
+echo $(cal)"
+
+echo files in current directory $(Ls)"
+
+echo "exit program..." break
+
+echo "enter a valid command"
+
+esac done
+
+dbda@dbda-virtualbox:-5./case.sh
